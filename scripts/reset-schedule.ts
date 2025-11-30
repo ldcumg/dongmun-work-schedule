2a58ef55ccaaee711edf34856b6ac5524e53a155
이원빈 <ldcumg@naver.com>
2025-12-01 00:30:47 +0900

    test: github action

--- scripts/reset-schedule.ts
+++ scripts/reset-schedule.ts
@@ -1,7 +1,7 @@
-console.log('script start');
-
 import admin from 'firebase-admin';
 import { Firebase } from '../src/constants';
+
+console.log('script start'); // ✅ 실행 확인용
 
 const serviceAccount = JSON.parse(
   Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT!, 'base64').toString()
@@ -13,11 +13,11 @@ admin.initializeApp({
     'https://dongmun-work-schedule-default-rtdb.asia-southeast1.firebasedatabase.app',
 });
 
-
 (async () => {
-  const db = admin.database();
   console.log('Deleting schedule...');
+  const db = admin.database();
+
   await db.ref(Firebase.SCHEDULE).remove();
+
   console.log('RTDB 초기화 완료');
-  process.exit(0);
 })();
