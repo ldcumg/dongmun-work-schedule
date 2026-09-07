f3e81eedd8023a75eac14bd8bd13e7761faca68c
이원빈 <ldcumg@naver.com>
2026-09-07 20:36:36 +0900

    chore: reset-schedule.ts -> reset-schedule.js

--- scripts/reset-schedule.js
+++ scripts/reset-schedule.js
@@ -1,25 +1,25 @@
-import admin from 'firebase-admin';
-import { Firebase } from '../src/constants';
+import admin from "firebase-admin";
+import { Firebase } from "../src/constants";
 
 (async () => {
   try {
     // 1. 환경변수 체크
     if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-      throw new Error('FIREBASE_SERVICE_ACCOUNT 환경변수가 없습니다.');
+      throw new Error("FIREBASE_SERVICE_ACCOUNT 환경변수가 없습니다.");
     }
 
     // 2. 서비스 계정 키 디코딩
     const serviceAccount = JSON.parse(
-      Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString(
-        'utf-8'
-      )
+      Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString(
+        "utf-8",
+      ),
     );
 
     // 3. Firebase 초기화
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
       databaseURL:
-        'https://dongmun-work-schedule-default-rtdb.asia-southeast1.firebasedatabase.app',
+        "https://dongmun-work-schedule-default-rtdb.asia-southeast1.firebasedatabase.app",
     });
 
     const db = admin.database();
